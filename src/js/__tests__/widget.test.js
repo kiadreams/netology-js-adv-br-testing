ecc303b60c9733e550e99e3376bb99e38604b63b
Igor Kobelev <kia1809@yandex.ru>
2025-06-08 16:45:37 +0300

    jsdom test has been added

---
+++ src/js/__tests__/widget.test.js
@@ -0,0 +1,17 @@
+/** @jest-environment jsdom */
+
+import ValidateCardWidget from '../ValidateCardWidget';
+
+
+test('test validate card widget', () => {
+  document.body.innerHTML = '<div class="container"></div>';
+
+  new ValidateCardWidget(document.querySelector('.container'));
+  const form = document.querySelector('form');
+  const innerHTML =
+    '<input id="card_number" type="text" name="card_number" placeholder="номер карты" class="input-field">' +
+    '<button id="submit-button" type="submit" class="submit-button">Проверить номер карты</button>';
+
+  expect(form.innerHTML).toEqual(innerHTML);
+});
+
